bd9d2f5164e67fe7122b0071163f6e7879510b68
Christopher Capito <ccapito@communitylife.degit config user.email ccapito@communitylife.d>
2021-03-09 17:13:50 +0100

    +

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,58 +2,83 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
 import PreviewCompatibleImage from './PreviewCompatibleImage'
+class Faq extends React.Component {
+    render() {
+        const { data } = this.props
+        const { edges: entries } = data.allMarkdownRemark
 
-// class Faq extends React.Component {
-//   render() {
-//     //const { data } = this.props
-//    // const { edges: posts } = data.allMarkdownRemark
-
-//     return (
-//       <div className="columns is-multiline">
-//         <p>Hello World</p>
-//       </div>
-//     )
-//   }
-// }
-
-const Faq = () => {
-  return (
-    <div>
-      <ul style={{listStyle:'none'}}>
-        <li>
-          <header>
-        <p className="post-meta">
-                    <Link
-                      className="title has-text-primary is-size-4"
-                      to="#"
-                    >
-                      Was ist eigentlich Risiko?
-                    </Link>
-                  </p>
-          </header>
-        </li>
-      </ul>
-    </div>
-  )
+        return (
+            <div>
+                <ul style={{ listStyle: 'none' }}>
+                    {entries &&
+                        entries.map(({ node: entry }) => (
+                            <li>
+                                <header>
+                                    <p className="post-meta">
+                                        <Link
+                                            className="title has-text-primary is-size-4"
+                                            to="#"
+                                        >
+                                            {entry.frontmatter.title}
+                                        </Link>
+                                    </p>
+                                </header>
+                                <video width="320" height="240" controls>
+                                    <source
+                                        src={entry.frontmatter.featuredVideo}
+                                        type="video/mp4"
+                                    />
+                                    Your browser does not support the video tag.
+                                </video>
+                                <p>{entry.excerpt}</p>
+                                <Link className="button" to={entry.fields.slug}>
+                                    Mehr lesen
+                                </Link>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default Faq
+export default () => (
+    <StaticQuery
+        query={graphql`
+            query FaqEntries {
+                allMarkdownRemark(
+                    filter: {
+                        frontmatter: { templateKey: { eq: "faq-entry" } }
+                    }
+                ) {
+                    edges {
+                        node {
+                            excerpt
+                            frontmatter {
+                                title
+                                date
+                                featuredVideo
+                            }
+                            headings {
+                                id
+                            }
+                            rawMarkdownBody
+                            fields {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `}
+        render={(data, count) => <Faq data={data} count={count} />}
+    />
+)
 
-
-// Faq.propTypes = {
-//   data: PropTypes.shape({
-//     allMarkdownRemark: PropTypes.shape({
-//       edges: PropTypes.array,
-//     }),
-//   }),
-// }
-
-// export default () => (
-//   <StaticQuery
-//     query={graphql`
-//       query FaqQuery {
-//       }
-//     `}
-//     render={(data, count) => <Faq data={data} count={count} />}
-//   />
-// )
+Faq.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            //edges: PropTypes.array,
+        }),
+    }),
+}
